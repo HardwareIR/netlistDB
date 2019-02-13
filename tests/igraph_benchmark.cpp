7de6d831651ac1320d23edd529c5f250b08bd269
Nic30 <nic30@seznam.cz>
2019-02-13 19:35:39 +0100

    codestyle: rm comented code

--- tests/igraph_benchmark.cpp
+++ tests/igraph_benchmark.cpp
@@ -31,7 +31,6 @@ igraph_bool_t bfs_callback(const igraph_t *graph,
 			   igraph_integer_t rank,
 			   igraph_integer_t dist,
 			   void *extra) {
-  //printf(" %li", (long int) vid);
 	//usleep(1);
 	(*reinterpret_cast<size_t*>(extra))++;
 	return 0;
