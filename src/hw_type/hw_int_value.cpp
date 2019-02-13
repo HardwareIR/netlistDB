e1dc5f7f13b19280377d5aa9daebaaf691abaa4d
Nic30 <nic30@seznam.cz>
2019-02-13 17:22:46 +0100

    codestyle

--- src/hw_type/hw_int_value.cpp
+++ src/hw_type/hw_int_value.cpp
@@ -6,9 +6,11 @@ namespace hw_type {
 HwIntValue::HwIntValue(const HwInt & t, uint64_t value) :
 		t(t), value(value), mask(t.all_mask) {
 }
+
 HwIntValue::HwIntValue(const HwInt & t, uint64_t value, uint64_t mask) :
 		t(t), value(value), mask(t.all_mask) {
 }
+
 HwIntValue::HwIntValue(const HwInt & t,
 		const boost::multiprecision::cpp_int & value,
 		const boost::multiprecision::cpp_int & mask) :
