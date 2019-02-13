05ae8895c92460f0ce502796cf1f880869a42bde
Nic30 <nic30@seznam.cz>
2019-02-13 17:04:19 +0100

    placeholder for normalization transformation

---
+++ src/transform/normalize.cpp
@@ -0,0 +1,12 @@
+#include "normalize.h"
+#include <exception>
+
+namespace netlistDB {
+namespace transform {
+
+bool TransformNormalize::apply(Netlist & ctx) {
+	throw std::runtime_error("not implemented");
+}
+
+}
+}
