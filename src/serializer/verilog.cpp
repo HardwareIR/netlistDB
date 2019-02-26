4e6e4e6f7b8224cd639181e0cb14f89e161d8053
Nic30 <nic30@seznam.cz>
2019-02-26 23:32:26 +0100

    add Verilog2001::indent

--- src/serializer/verilog.cpp
+++ src/serializer/verilog.cpp
@@ -8,9 +8,9 @@ void Verilog2001::serialize(Netlist & netlist, std::ostream & str) {
 }
 
 void Verilog2001::indent(size_t cnt, std::ostream & str) {
-	for (size_t i = 0; i < cnt; i++) {
-		str << INDENT;
-	}
+//	for (size_t i = 0; i < cnt; i++) {
+//		str << INDENT;
+//	}
 }
 
 
