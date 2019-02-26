b9272606ae09883433f3c765edbbaa9ff96adf85
Nic30 <nic30@seznam.cz>
2019-02-26 09:52:45 +0100

    verilog: indent method

--- include/netlistDB/serializer/verilog.h
+++ include/netlistDB/serializer/verilog.h
@@ -11,6 +11,7 @@ class Verilog2001: public Serializer {
 public:
 	Verilog2001();
 	virtual void serialize(Netlist & netlist, std::ostream & str) override;
+	void indent(size_t cnt, std::ostream & str);
 
 };
 
--- src/serializer/verilog.cpp
+++ src/serializer/verilog.cpp
@@ -7,5 +7,12 @@ void Verilog2001::serialize(Netlist & netlist, std::ostream & str) {
 
 }
 
+void Verilog2001::indent(size_t cnt, std::ostream & str) {
+	for (size_t i = 0; i < cnt; i++) {
+		str << INDENT;
+	}
+}
+
+
 }
 }
