900b5c44df729ab7815fc1b787f26f791a72fe14
Nic30 <nic30@seznam.cz>
2019-03-02 13:16:11 +0100

    HwInt codestyle

--- include/netlistDB/hw_type/hw_int.h
+++ include/netlistDB/hw_type/hw_int.h
@@ -12,15 +12,11 @@ namespace hw_type {
 
 class HwInt;
 
-/* Container of the value for integer type
+/* Container of the value for hardware integer type
  **/
 class HwIntValue: public iHwTypeValue {
 public:
 	using aint_t = boost::multiprecision::cpp_int;
-
-	HwIntValue(const HwInt & t, uint64_t val);
-	HwIntValue(const HwInt & t, uint64_t val, uint64_t mask);
-	HwIntValue(const HwInt & t, const aint_t & value, const aint_t & mask);
 
 	// backward reference to type
 	// (to keep track of the sign and size, ...)
@@ -32,25 +28,36 @@ public:
 	// mask of size of the type, if the bit is 0 it means value
 	// is undefined
 	aint_t mask;
+
+	HwIntValue(const HwInt & t, uint64_t val);
+	HwIntValue(const HwInt & t, uint64_t val, uint64_t mask);
+	HwIntValue(const HwInt & t, const aint_t & value, const aint_t & mask);
 };
 
+/*
+ * Basic hardware integer type with specified size and sign
+ * */
 class HwInt: public iHwType {
 	// size of value in bits
 	const size_t _M_bit_length;
 
 public:
+	// value class used by this type class
+	using value_type = HwIntValue;
+
+	// the mask value for the type of this size
 	const boost::multiprecision::cpp_int all_mask;
 
-	using value_type = HwIntValue;
 	// true if the value is signed integer
 	const size_t is_signed;
 	// if false vector of size 1 is converted to scalar in HDL
 	// (affect only the representation in final HDL)
 	const bool has_to_be_vector;
 
+	HwInt(size_t bit_length, bool is_signed = false, bool has_to_be_vector = false);
+
 	virtual size_t bit_length() const override;
 	virtual bool operator==(const iHwType & other) const override;
-	HwInt(size_t bit_length, bool is_signed, bool has_to_be_vector = false);
 
 	~HwInt();
 };
