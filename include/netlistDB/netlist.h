88387ea7dfc331c529807dc97e760717461f4b00
Nic30 <nic30@seznam.cz>
2019-03-04 00:27:20 +0100

    netlist.h extract sensitivity_ctx, iNode

--- include/netlistDB/netlist.h
+++ include/netlistDB/netlist.h
@@ -15,14 +15,16 @@
 #include <unordered_map>
 #include <atomic>
 
+#include <netlistDB/inode.h>
 #include <netlistDB/utils/ordered_set.h>
 #include <netlistDB/varId.h>
 #include <netlistDB/constants.h>
 #include <netlistDB/pointer_container.h>
 #include <netlistDB/function_def.h>
-#include <netlistDB/utils/chained_iterator.h>
 #include <netlistDB/hw_type/ihw_type.h>
 #include <netlistDB/hw_type/ihw_type_value.h>
+#include <netlistDB/hw_type/hw_int.h>
+#include <netlistDB/utils/sensitivity_ctx.h>
 
 namespace netlistDB {
 
@@ -30,64 +32,7 @@ class Net;
 class Netlist;
 class Statement;
 
-/**
- * Inteface for nodes in database
- *
- **/
-class iNode {
-public:
-	// sequential number used as a id during serialization
-	// and as a index in main list of iNode instances in Netlist.nodes
-	size_t index;
-
-	using iterator = utils::ChainedSequence<iNode*>;
-	using predicate_t = std::function<bool(iNode*)>;
-
-	// iterator of endpoints for Net or outputs for statement or result for expression
-	iterator forward;
-	// iterator of drivers for net or inputs for statement or args for expression
-	iterator backward;
-
-	virtual ~iNode() {
-	}
-};
-
 class OperationNode: public iNode {
-};
-
-/*
- * Sensitivity list used for resolution of sensitivity for hw statement instances
- *
- * @ivar contains_event_dep: true if this contains event dependent
- *    sensitivity
- */
-
-class SensitivityCtx: public utils::OrderedSet<iNode*> {
-public:
-	bool contains_event_dep;
-	SensitivityCtx() :
-			contains_event_dep(false) {
-	}
-
-	template<typename iterable>
-	void extend(const iterable & other) {
-		_extend<iterable>(other);
-	}
-
-	void clear() {
-		utils::OrderedSet<iNode*>::clear();
-		contains_event_dep = false;
-	}
-private:
-	template<typename iterable>
-	void _extend(const iterable & other) {
-		utils::OrderedSet<iNode*>::extend(other);
-	}
-	void _extend(const SensitivityCtx & other) {
-		utils::OrderedSet<iNode*>::extend(other);
-		contains_event_dep |= other.contains_event_dep;
-	}
-
 };
 
 /*
@@ -184,6 +129,75 @@ public:
 };
 
 /**
+ * Circuit graph representation
+ *
+ * There are multiple types of node in this directed graph
+ * The signal, statement and FunctionCall (which represents also the operator usage)
+ *
+ * Each node has own list of neighbors. This is the case because the graph
+ * is expected to change frequently and be larger than 10k nodes this representation
+ * by linked list or connection matrix would not be efficient.
+ *
+ * Also for some nodes the order of neighbors does matter for some does not.
+ * For example order of Signal endpoints does not matter. The order of Signals
+ * in FunctionCall does.
+ *
+ **/
+class Netlist {
+public:
+	// name for debugging purposes
+	std::string name;
+	std::vector<Net*> nets;
+	std::vector<iNode*> nodes;
+
+	Netlist(const Netlist & other) = delete;
+	Netlist(const std::string & name);
+
+	// create input signal
+	Net & sig_in(const std::string & name, hw_type::iHwType & t);
+	// create output signal
+	Net & sig_out(const std::string & name, hw_type::iHwType & t);
+
+	/* create constant net in this Netlist
+	 * @param v value of the constant net
+	 */
+	template<typename hw_type_t>
+	Net & const_net(hw_type_t & t, typename hw_type_t::value_type::aint_t v);
+
+	/* create constant net in this Netlist
+	 * @param v value of the constant net
+	 * @param mask mask for the value
+	 */
+	template<typename hw_type_t>
+	Net & const_net(hw_type_t & t, typename hw_type_t::value_type::aint_t v,
+			typename hw_type_t::value_type::aint_t mask);
+	// create internal signals without specified name
+	Net & sig(hw_type::iHwType & t);
+	// create internal signal with name specified
+	Net & sig(const std::string & name, hw_type::iHwType & t);
+
+	/*
+	 * Add node in to nodes on place specified by index and
+	 * optionally to nets as well
+	 **/
+	void register_node(iNode & n);
+	void register_node(Net & n);
+
+	/*
+	 * Remove node from nodes on place specified by index and
+	 * optionally from nets as well
+	 *
+	 * @note thread safe, but requires compaction to reduce memory footprint
+	 **/
+	void unregister_node(iNode & n);
+	void unregister_node(Net & n);
+
+	void integrty_assert();
+
+	~Netlist();
+};
+
+/**
  * Hyperedge which connects the the statements, expressions, etc.
  * */
 class Net: public iNode {
@@ -251,85 +265,42 @@ public:
 
 	// disconnect the selected endpoints
 	void forward_disconnect(iNode::predicate_t pred);
+private:
+	/*
+	 * Wrap c-constant to constant net in parent netlist
+	 * */
+	template<typename T>
+	Net & wrap_val_to_const_net(T val) {
+		auto int_t = dynamic_cast<typename hw_type::HwInt*>(&t);
+		if (int_t) {
+			return ctx.const_net(*int_t, val);
+		}
+		throw std::runtime_error(
+				std::string(__PRETTY_FUNCTION__)
+						+ "unknown type for automatic const instantiation");
+	}
 };
 
-/**
- * Circuit graph representation
- *
- * There are multiple types of node in this directed graph
- * The signal, statement and FunctionCall (which represents also the operator usage)
- *
- * Each node has own list of neighbors. This is the case because the graph
- * is expected to change frequently and be larger than 10k nodes this representation
- * by linked list or connection matrix would not be efficient.
- *
- * Also for some nodes the order of neighbors does matter for some does not.
- * For example order of Signal endpoints does not matter. The order of Signals
- * in FunctionCall does.
- *
- **/
-class Netlist {
-public:
-	// name for debugging purposes
-	std::string name;
-	std::vector<Net*> nets;
-	std::vector<iNode*> nodes;
+template<typename hw_type_t>
+Net & Netlist::const_net(hw_type_t & t,
+		typename hw_type_t::value_type::aint_t v) {
+	Net & n = sig("const_", t);
+	n.val = new typename hw_type_t::value_type(t, v, t.all_mask);
+	return n;
+}
 
-	Netlist(const Netlist & other) = delete;
-	Netlist(const std::string & name);
-
-	// create input signal
-	Net & sig_in(const std::string & name, hw_type::iHwType & t);
-	// create output signal
-	Net & sig_out(const std::string & name, hw_type::iHwType & t);
-
-	/* create constant net in this Netlist
-	 * @param v value of the constant net
-	 */
-	template<typename hw_type_t>
-	Net & const_net(hw_type_t & t, typename hw_type_t::value_type::aint_t v) {
-		Net & n = sig("const_", t);
-		n.val = new typename hw_type_t::value_type(t, v, t.all_mask);
-		return n;
-	}
-
-	/* create constant net in this Netlist
-	 * @param v value of the constant net
-	 * @param mask mask for the value
-	 */
-	template<typename hw_type_t>
-	Net & const_net(hw_type_t & t,
-			typename hw_type_t::value_type::aint_t v,
-			typename hw_type_t::value_type::aint_t mask) {
-		Net & n = sig("const_", t);
-		n.val = new typename hw_type_t::value_type(t, v, mask);
-		return n;
-	}
-	// create internal signals without specified name
-	Net & sig(hw_type::iHwType & t);
-	// create internal signal with name specified
-	Net & sig(const std::string & name, hw_type::iHwType & t);
-
-	/*
-	 * Add node in to nodes on place specified by index and
-	 * optionally to nets as well
-	 **/
-	void register_node(iNode & n);
-	void register_node(Net & n);
-
-	/*
-	 * Remove node from nodes on place specified by index and
-	 * optionally from nets as well
-	 *
-	 * @note thread safe, but requires compaction to reduce memory footprint
-	 **/
-	void unregister_node(iNode & n);
-	void unregister_node(Net & n);
-
-	void integrty_assert();
-
-	~Netlist();
-};
+/* create constant net in this Netlist
+ * @param v value of the constant net
+ * @param mask mask for the value
+ */
+template<typename hw_type_t>
+Net & Netlist::const_net(hw_type_t & t,
+		typename hw_type_t::value_type::aint_t v,
+		typename hw_type_t::value_type::aint_t mask) {
+	Net & n = sig("const_", t);
+	n.val = new typename hw_type_t::value_type(t, v, mask);
+	return n;
+}
 
 }
 
---
+++ include/netlistDB/utils/sensitivity_ctx.h
@@ -0,0 +1,40 @@
+#include <netlistDB/inode.h>
+#include <netlistDB/utils/ordered_set.h>
+
+namespace netlistDB {
+/*
+ * Sensitivity list used for resolution of sensitivity for hw statement instances
+ *
+ * @ivar contains_event_dep: true if this contains event dependent
+ *    sensitivity
+ */
+
+class SensitivityCtx: public utils::OrderedSet<iNode*> {
+public:
+	bool contains_event_dep;
+	SensitivityCtx() :
+			contains_event_dep(false) {
+	}
+
+	template<typename iterable>
+	void extend(const iterable & other) {
+		_extend<iterable>(other);
+	}
+
+	void clear() {
+		utils::OrderedSet<iNode*>::clear();
+		contains_event_dep = false;
+	}
+private:
+	template<typename iterable>
+	void _extend(const iterable & other) {
+		utils::OrderedSet<iNode*>::extend(other);
+	}
+	void _extend(const SensitivityCtx & other) {
+		utils::OrderedSet<iNode*>::extend(other);
+		contains_event_dep |= other.contains_event_dep;
+	}
+
+};
+
+}
