5256b6cfbe9a871a9c71fea6f6404756ef923ce0
Nic30 <nic30@seznam.cz>
2019-03-04 00:39:31 +0100

    verilog serializer: new test mux with elif

--- tests/verilog_simple_modules_test.cpp
+++ tests/verilog_simple_modules_test.cpp
@@ -82,6 +82,58 @@ BOOST_AUTO_TEST_CASE( simple_ff_module ) {
 	}
 }
 
+
+BOOST_AUTO_TEST_CASE( mux_module ) {
+	Netlist ctx("mux_module");
+
+	auto &sel = ctx.sig_in("sel", hw_int8);
+	auto &a_in0 = ctx.sig_in("a_in0", hw_int32);
+	auto &a_in1 = ctx.sig_in("a_in1", hw_int32);
+	auto &a_in2 = ctx.sig_in("a_in2", hw_int32);
+	auto &a_in3 = ctx.sig_in("a_in3", hw_int32);
+	auto &a_out = ctx.sig_out("a_out", hw_int32);
+
+	If(sel == 0) (&a_out(a_in0))
+	.Elif(sel == 1, &a_out(a_in1))
+	.Elif(sel == 2, &a_out(a_in2))
+	.Elif(sel == 3, &a_out(a_in3));
+
+	TransformToHdlFriendly t;
+	t.apply(ctx);
+
+	Verilog2001 ser;
+	{
+		stringstream str;
+		stringstream ref;
+		ref << "module mux_module(" << endl;
+		ref << "    input a_in0," << endl;
+		ref << "    input a_in1," << endl;
+		ref << "    input a_in2," << endl;
+		ref << "    input a_in3," << endl;
+		ref << "    output reg a_out," << endl;
+		ref << "    input sel);" << endl;
+        ref << endl;
+		ref << "    always @(sel or a_in0 or a_in1 or a_in2 or a_in3) begin: diver_of_a_out" << endl;
+		ref << "        if (sel == 8'h00)" << endl;
+		ref << "            a_out = a_in0;" << endl;
+		ref << "        else if (sel == 8'h01)" << endl;
+		ref << "            a_out = a_in1;" << endl;
+		ref << "        else if (sel == 8'h02)" << endl;
+		ref << "            a_out = a_in2;" << endl;
+		ref << "        else if (sel == 8'h03)" << endl;
+		ref << "            a_out = a_in3;" << endl;
+		ref << endl;
+		ref << "    end" << endl;
+        ref << endl;
+		ref << "endmodule";
+
+		ser.serialize(ctx, str);
+		//cerr << str.str() << endl;
+		BOOST_CHECK_EQUAL(str.str(), ref.str());
+	}
+}
+
+
 //____________________________________________________________________________//
 
 BOOST_AUTO_TEST_SUITE_END()
