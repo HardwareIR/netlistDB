4e26ed99d4c9f1e13b5124976aac2c932edf174e
Nic30 <nic30@seznam.cz>
2019-03-06 23:34:22 +0100

    msvc support for static lib build

--- include/netlistDB/common.h
+++ include/netlistDB/common.h
@@ -1,11 +1,17 @@
 #pragma once
 
 #ifdef _WIN32
+#ifdef NETLISTDB_DLL
 	#ifdef NETLISTDB_EXPORTS
 		#define NETLISTDB_PUBLIC __declspec(dllexport)
 	#else
 		#define NETLISTDB_PUBLIC __declspec(dllimport)
+	#endif
+#else
+	#define NETLISTDB_PUBLIC
 #endif
+
+
 #else
 	#define NETLISTDB_PUBLIC
 #endif
